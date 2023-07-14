023a8959cee59e9b966ff11a72c272a62503ecfd
atroopa <atroopa@gmail.com>
2023-07-14 20:03:38 +0330

    save a new test

--- tests.sql
+++ tests.sql
@@ -2,3 +2,4 @@ use teamyar;
 
 CALL InsertOrUpdateInvoice(1, 2, 3, 100, '2023-07-10');
 CALL GetCustomerPurchases('مانیتور', '2023-07-01', '2023-07-10');
+CALL GetTopSellingProducts(20, 40, '2023-07-01', '2023-07-31');
